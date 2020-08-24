b7436f32a0647501e291039bfda5034f6002ac19
Wemerson Silva <wemersonblend@gmail.com>
2020-08-24 16:47:59 -0300

    Organização arquivos na pasta

--- assets/main.js
+++ assets/main.js
@@ -1,131 +1,78 @@
-var firstValue = null;
-var operation = null;
-var secondValue = null;
-var calcResult = null;
+var primaryMemory = null;
+var secondMemory = null;
+var currentMemory = 1;
+var typeOfOperation = null;
+var result = null;
 
-function addition() {
-
+function operation(operation) {
+	typeOfOperation = operation;
+	changeMemoryPosition();
 }
 
-function subtraction() {}
+function equal() {
 
-function division() {}
+	primaryMemory *= 1;
+	secondMemory *= 1;
 
-function multiplication() {}
-function porcentage() {}
+	if(typeOfOperation == "addition") {
+		result = primaryMemory + secondMemory;
+	}
 
-function equal() {}
+	if(typeOfOperation == "subtraction") {
+		result = primaryMemory - secondMemory;
+	}
 
-function clear() {
-	firstValue = null;
-	secondValue = null;
-	calcResult = null;
+	if(typeOfOperation == "division") {
+		result = primaryMemory / secondMemory;
+	}
 
+	if(typeOfOperation == "multiplication") {
+		result = primaryMemory * secondMemory;
+	}
+
+	console.log(typeOfOperation, primaryMemory, secondMemory, result)
+
+	document.querySelector("#display").innerHTML = result;
 }
 
-function digit() {
+function clearDisplay() {
+	console.log('clear');
+	primaryMemory = null;
+	secondMemory = null;
+	currentMemory = 1;
+	typeOfOperation = null;
+	result = null;
+	document.querySelector("#display").innerHTML = 0;
+}
+
+function changeMemoryPosition() {
+
+	console.log(primaryMemory, secondMemory)
+
+	if(currentMemory == 1) {
+		currentMemory = 2;
+	} else {
+		currentMemory = 1;
+	}
+
+	document.querySelector("#display").innerHTML = 0;
+}
+
+function digit(num) {
+
+	var displayedNum = document.querySelector("#display").innerHTML;
 	
+	if(displayedNum == 0) {
+		displayedNum = "";
+	}
+	
+	displayedNum += "" + num;
+
+	if( currentMemory == 1 ) {
+		primaryMemory = displayedNum;
+	} else {
+		secondMemory = displayedNum;
+	}
+
+	document.querySelector("#display").innerHTML = displayedNum;
 }
-
-function turnOn() {}
-function turnoff() {}
-
-
-
-
-
-// document.querySelector(".number1").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("1")
-// }
-// document.querySelector(".number2").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("2")
-// }
-// document.querySelector(".number3").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("3")
-// }
-// document.querySelector(".number4").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("4")
-// }
-// document.querySelector(".number5").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("5")
-// }
-// document.querySelector(".number6").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("6")
-// }
-// document.querySelector(".number7").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("7")
-// }
-// document.querySelector(".number8").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("8")
-// }
-// document.querySelector(".number9").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("9")
-// }
-// document.querySelector(".number0").onclick = function(){
-// 	document.querySelector(".visor").innerHTML += ("0")
-// }
-
-// /*VARIAVEIS*/
-// let fator1;
-// let fator2;
-// let resultado;
-// let operacao;
-
-// /*PROCESSAMENTO*/
-// document.querySelector(".limp").onclick = function(){
-// 	document.querySelector(".visor").innerHTML = ""
-// 	fator1 = ""
-// 	fator2 = ""
-// 	resultado = ""
-// 	operacao = ""
-// }
-// document.querySelector(".mult").onclick = function(){
-//  fator1 = document.querySelector(".visor").innerHTML;
-// 	console.log (fator1);
-// 	document.querySelector(".visor").innerHTML = ""
-// 	operacao = "mult"
-// }
-// document.querySelector(".divisao").onclick = function(){
-//  fator1 = document.querySelector(".visor").innerHTML;
-// 	console.log (fator1);
-// 	document.querySelector(".visor").innerHTML = ""
-// 	operacao = "div"
-// }
-// document.querySelector(".sub").onclick = function(){
-//  fator1 = document.querySelector(".visor").innerHTML;
-// 	console.log (fator1);
-// 	document.querySelector(".visor").innerHTML = ""
-// 	operacao = "sub"
-// }
-// document.querySelector(".sum").onclick = function(){
-//  fator1 = document.querySelector(".visor").innerHTML;
-// 	console.log (fator1);
-// 	document.querySelector(".visor").innerHTML = ""
-// 	operacao = "sum"
-// }
-// document.querySelector(".percent").onclick = function(){
-//  fator1 = document.querySelector(".visor").innerHTML;
-// 	console.log (fator1);
-// 	document.querySelector(".visor").innerHTML = ""
-// 	operacao = "percent"
-// }
-// document.querySelector(".igual").onclick = function(){
-// 	fator2 = document.querySelector(".visor").innerHTML;
-// 	if(operacao == "mult"){
-// 		resultado = fator1*fator2;
-// 	}
-// 	else if (operacao == "div") {
-// 		resultado = fator1/fator2;
-// 	}
-// 	else if (operacao = "sub"){
-// 		resultado = fator1-fator2;
-// 	}
-// 	else if (operacao = "sum"){
-// 		resultado = fator1+fator2;
-// 	}
-// 	else if (operacao = "percent"){
-// 		resultado = fator1/fator2*100;
-// 	}
-// 	console.log(resultado);
-// 	document.querySelector(".visor").innerHTML = resultado;	
-// }
